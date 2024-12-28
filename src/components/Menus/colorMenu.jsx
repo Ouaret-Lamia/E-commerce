3874503e05118900dc1b7ca3c9b6616a3595802d
Ouaret Lamia <ouaretlamia04@gmail.com>
2024-12-28 19:41:59 +0100

    ok

--- src/components/Menus/colorMenu.jsx
+++ src/components/Menus/colorMenu.jsx
@@ -1,42 +1,32 @@
-import './langMenu.css'
+import './langMenu.css';
 import React, { useState } from 'react';
-import { IoMdArrowDropdown } from 'react-icons/io'; // For the dropdown arrow
 
-export default function ColorMenu({ colors = [], onColorSelect }){
-  const [isOpen, setIsOpen] = useState(false);
-  const [selectedColor, setSelectedColor] = useState('All Colors'); 
+export default function ColorMenu({ colors = [], onColorSelect }) {
+  const [selectedColor, setSelectedColor] = useState('All Colors');
 
   // Filter out any undefined or null values
-  const validColors = colors.filter(color => color);
+  const validColors = colors.filter((color) => color);
 
-  const colorOptions = [
-    ...validColors.map(color => ({ name: color }))
-  ];
-
-  const toggleDropdown = () => setIsOpen(!isOpen);
-
-  const selectColor = (colorName) => {
+  const handleChange = (event) => {
+    const colorName = event.target.value;
     setSelectedColor(colorName);
-    setIsOpen(false);
-    onColorSelect(colorName);  // Call the passed function with the selected color
-    // Here you would typically call a function to filter by color
+    onColorSelect(colorName); // Call the passed function with the selected color
   };
 
   return (
     <div className="language-dropdown">
-      <button onClick={toggleDropdown} className="dropdown-toggle">
-        <span>{selectedColor}</span>
-        <IoMdArrowDropdown />
-      </button>
-      {isOpen && (
-        <ul className="dropdown-menu">
-          {colorOptions.map((color) => (
-            <li key={color.name} onClick={() => selectColor(color.name)}>
-              {color.name}
-            </li>
-          ))}
-        </ul>
-      )}
+      <select
+        value={selectedColor}
+        onChange={handleChange}
+        className="dropdown-toggle"
+      >
+        {/* <option value="All Colors">All Colors</option> */}
+        {validColors.map((color) => (
+          <option key={color} value={color}>
+            {color}
+          </option>
+        ))}
+      </select>
     </div>
   );
-};
+}
